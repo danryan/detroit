da36ce633f58ef6d87b1d96086b06605f1357948
Dan Ryan <scriptfu@gmail.com>
2011-01-06 21:29:56 -0500

    trying out sinatra/respond_to

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ gem 'haml'
 gem "rrd-ffi", :require => "rrd"
 gem "i18n"
 gem "sinatra-reloader", :require => "sinatra/reloader"
+gem "sinatra-respond_to", :require => "sinatra/respond_to"
--- app.rb
+++ app.rb
@@ -1,62 +1,84 @@
 module Detroit
   class App < Sinatra::Base    
+    register Sinatra::RespondTo
+    
     configure(:development) do
       register Sinatra::Reloader
-    end
-    
-    def get_data(host, plugin, metric, options={})
-      rrd = RRD::Base.new(File.join(RRD_PATH, host, plugin, "#{metric}.rrd"))
-      start_at = options[:start_at] || rrd.starts_at
-      end_at = options[:end_at] || rrd.ends_at
-      results = rrd.fetch(:average, :start => start_at, :end => end_at)
-      r = Array.new
-      
-      index = results.shift
-      index.each do |i|
-        r << {:label => i, :data => [] }
-      end
-      
-      results.each do |line|
-        line.map! { |a| a.kind_of?( Fixnum ) ? a : 0 }
-        index.each_with_index do |v,i|
-          r[i][:data] << line[i]
-        end
-      end
-      r
     end
     
     RRD_PATH = "#{File.dirname(__FILE__)}/data/rrd/"
     
     get "/" do
       @hosts = Dir.glob("#{RRD_PATH}/*").map { |h| File.basename(h) }
-      haml :index
+      respond_to do |wants|
+        wants.html { haml :hosts }
+        wants.json do 
+          { :hosts => @hosts }.to_json
+        end
+      end
     end
     
     get "/:host" do
-      
-      @host = params[:host]
+      @host = params[:host].gsub("_",".")
       @plugins = Dir.glob("#{RRD_PATH}/#{@host}/*").map { |p| File.basename(p) }
-      haml :host
+      respond_to do |wants|
+        wants.html { haml :host }
+        wants.json do
+          { :host => @host, :plugins => @plugins }.to_json
+        end
+      end
     end
     
     get "/:host/:plugin" do
-      @host = params[:host]
+      @host = params[:host].gsub("_",".")
       @plugin = params[:plugin]
       @metrics = Dir.glob("#{RRD_PATH}/#{@host}/#{@plugin}/*.rrd").map do |m|
         File.basename(m).sub(".rrd","")
       end
-      haml :plugin
+      respond_to do |wants|
+        wants.html { haml :plugin }
+        wants.json do
+          { :host => @host, :plugin => @plugin, :metrics => @metrics }.to_json
+        end
+      end
     end
 
     
     get "/:host/:plugin/:metric" do
-      # content_type :json
-      @host = params[:host]
+      @host = params[:host].gsub("_",".")
       @plugin = params[:plugin]
       @metric = params[:metric]
-      data = get_data(@host, @plugin, @metric)
-      # {:one => 1, :two => 2}.to_json
-      data.to_json
+      @data = get_data(@host, @plugin, @metric)
+      respond_to do |wants|
+        wants.html { haml :metric }
+        wants.json do
+          { :host => @host, :plugin => @plugin, 
+            :metric => @metric, :data => @data }.to_json
+        end
+      end
+    end
+    
+    private
+    
+    def get_data(host, plugin, metric, options={})
+      rrd = RRD::Base.new(File.join(RRD_PATH, host, plugin, "#{metric}.rrd"))
+      start_at = options[:start_at] || Time.now - 3600.seconds    # rrd.starts_at
+      end_at = options[:end_at] || Time.now                       # rrd.ends_at
+      results = rrd.fetch(:average, :start => start_at, :end => end_at)
+      data = Array.new
+      
+      index = results.shift
+      index.each do |i|
+        data << {:label => i, :data => [] }
+      end
+      
+      results.each do |line|
+        line.map! {|a| a.respond_to?('nan?') ? ( a.nan? ? 0 : a ) : a }
+        index.each_with_index do |v,i|
+          data[i][:data] << line[i]
+        end
+      end
+      data
     end
   end
 end
